eededee6c20e42198b41bbd98ac8f395be831eb1
Usha Annipu <uannipu@gmail.com>
2013-12-22 20:22:18 -0500

    login screen fix

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -26,16 +26,4 @@
 
         </form>
 </div>
-    <div class="description">
-        <h1>Welcome to the Testing Site</h1>
-        <p> This testing site is forum to share your experience and tips with various tests that we offer, helps you prepare ahead of the tests.
-        </p>
-        <p>This site allows users to register, follow other users, post a topic regarding various testing programs that we currently support, such as GRE, TOEFL,SAT etc..</p>
-    </div>
-    <table id="details">
-        <tr>
-            <td> The +1 features that I added are "Edit a post", "Delete a post", "Update Profile".</td>
-            <td> This is strictly for testing purposes and not for any other business purposes.</td>
-            <td>If you are looking for another testing program, this is probably not the right web site that you are looking for.</td>
-        </tr>
-    </table>
+  
